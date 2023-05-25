cc5414169472dd6654706a1a597624e2e95ee8af
iugorsette <iugorsette@gmail.com>
2023-05-25 17:02:45 -0300

    refactor: change footer to prepare background

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,12 +1,17 @@
+import Image from "next/image";
 import CarouselComponent from "./Corousel";
+
+import picture from "../../../public/picture/sette.png";
 
 export function Footer() {
   return (
-    <>
-    <CarouselComponent />
-    <div className="flex h-8 justify-center items-center bg-gradient-to-b from-zinc-800 to-green-local-500">
+    <div className="flex ">
+      <div className="flex h-auto justify-center items-center bg-gradient-to-b from-zinc-800 to-green-local-500 relative">
+        <CarouselComponent />
+        <div>
+          <Image src={picture} width={144} />
+        </div>
+      </div>
     </div>
-    </>
-    
   );
 }
