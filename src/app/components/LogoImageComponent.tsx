40b8907c03664d9b6a2d655c541a550517674e08
iugorsette <iugorsette@gmail.com>
2023-07-20 00:58:46 -0300

    refactor: improments a LogoImageComponent ang implements new view qhen hover

--- src/app/components/LogoImageComponent.tsx
+++ src/app/components/LogoImageComponent.tsx
@@ -5,15 +5,17 @@ interface LogoImageComponentProps {
   size: number;
 }
 
-export function LogoImageComponent(props: LogoImageComponentProps) {
-  return (
-    <div className="rounded-full border-2 border-green-local-500 bg-green-local-500 ">
-      <Image
-        width={props.size}
-        src={props.foto}
-        alt=""
-        className="rounded-full bg-green-local-500"
-      />
-    </div>
-  );
-}
+
+export function LogoImageComponent(props:LogoImageComponentProps) {
+    return (
+      <div className="relative rounded-full overflow-hidden border-2 border-green-local-500 group">
+        <Image
+          width={props.size}
+          src={props.foto}
+          alt=""
+          className="rounded-full bg-green-local-500 group-hover:bg-green-local-300 transition-all duration-500 ease-in-out"
+        />
+        </div>
+    );
+  }
+  
