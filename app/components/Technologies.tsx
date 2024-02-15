273897cdaeb87675fc038d58cb93bda0b488c420
iugorsette <iugorsette@gmail.com>
2024-02-15 16:57:05 -0300

    refactor: improve texts to my techs

--- app/components/Technologies.tsx
+++ app/components/Technologies.tsx
@@ -22,49 +22,49 @@ export function Technologies() {
       stack: "AngularJS",
       url: "https://github.com/iugorsette/warehouse-frontend",
       description:
-        "I built a web application using AngularJS, that's app for Warehouse management system (WMS) and you can see on my github.",
+        "I developed a web application using AngularJS, that is the app for Warehouse management system (WMS), and you can see it on my GitHub.",
     },
     {
       name: "Warehouse backend",
       stack: "NestJs",
       url: "https://github.com/iugorsette/warehouse-backend",
       description:
-        "I built a back-end web application using NestJs and MySQL, that's app for Warehouse management system (WMS) and you can see on my github.",
+        "I developed a back-end web application using NestJs and MySQL, that's an app for Warehouse management system (WMS), and you can see it on my GitHub.",
     },
     {
       name: "React Gnome Calculator",
       stack: "ReactJs",
       url: "https://github.com/iugorsette/react-calculator",
       description:
-        "For study purposes, I replicated the gnome calculator with some features using react and you can see on my github",
+        "For academic purposes, I replicated the gnome calculator with some features using react, and you can see it on my GitHub.",
     },
     {
       name: "Pizzapp",
       stack: "React Native",
       url: "https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2023-2-e3-proj-mov-t2-pmv-ads-2023-2-e3-proj-mov-t2-pizzapp",
       description:
-        "I have ever built a web application using React Native in my university, is a mobile application for Pizza Delivery and you can see on my github.",
+        "I created an app using React Native for school purposes. It is a mobile app for Pizza delivery, and you can find it on my GitHub.",
     },
     {
       name: "Task app frontend",
       stack: "ReactJs",
       url: "https://github.com/iugorsette/task-login-frontend",
       description:
-        "I built a web application using ReactJs, that's app for Task management system and you can see on my github.",
+        "I developed a web application using ReactJs to be used as a Task management system, as you can see on my GitHub.",
     },
     {
       name: "Task app backend",
       stack: "NodeJS",
       url: "https://github.com/iugorsette/task-login-backend",
       description:
-        "I create and implement new features performing process automation, as well as working on troubleshooting nodejs applications, I am still working on it.",
+        "I created and improved a backend API to manage tasks and serve to the frontend, as you can see on my GitHub.",
     },
     {
       name: "Rocypt",
       stack: ".NET C#",
       url: "https://github.com/ICEI-PUC-Minas-PMV-ADS/pmv-ads-2023-1-e2-proj-int-t6-rocypt",
       description:
-        "I contributed to a password manager API project developed in .NET C#. Throughout the project, we employed agile methodologies to enhance collaboration and flexibility in our development process.",
+        "I contributed to a password manager API project developed in .NET C#. We employed agile methodologies throughout the project to enhance collaboration and flexibility in our development process.",
     },
   ];
 
